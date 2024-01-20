00ce7e1d0647f5f4e39c7caefa1069c53a9e84dd
Alex <wiggywunka@gmail.com>
2024-01-20 13:36:22 -0800

    adding web files

---
+++ form_send.php
@@ -0,0 +1,21 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "fabriziomartinez1217@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $firstname . " " . $lastname . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $firstname . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    //echo "Mail Sent. Thank you " . $firstname . ", we will contact you shortly. Please click <a href='./'>Here</a> to go back to the homepage.";
+    header('Location: form-sent.html');
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
